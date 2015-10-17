a66a99488211841952e2ec8763a72363aa05ef05
U-ownerpc\OWNER <bunchhieng@gmail.com>
2015-10-17 16:18:49 -0400

    Solved

---
+++ src/LargeSum.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * Created by OWNER on 10/17/2015.
+ */
+public class LargeSum {
+    public static void main(String[] args) {
+        int sum = 0;
+        Scanner in = new Scanner(System.in);
+        BigInteger n = new BigInteger(in. sum);
+    }
+}
---
+++ src/OneDArray.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Created by OWNER on 10/17/2015.
+ */
+public class OneDArray {
+    public static void main(String[] args) {
+        int count = 0;
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+
+        }
+    }
+}
