c9d7fac0bb00d4331d19538b6abfa4d55b3c48d1
Bunchhieng Soth <Bunchhieng@Bunchhiengs-MacBook-Pro.local>
2015-10-16 20:38:33 -0400

    fixed untracked files

--- src/Datatypes.java
+++ src/Datatypes.java
@@ -14,9 +14,9 @@ public class Datatypes {
                 if (x >= -128 && x <= 127) System.out.println("* byte");
                 if (x >= -32768 && x <= 32767) System.out.println("* short");
                 if (x >= -Math.pow(2, 31) && x <= Math.pow(2, 31) - 1) System.out.println("* int");
-                if (x >= -Math.pow(2, 63) && x <= Math.pow(2, 63)-1) System.out.println("* long");
+                if (x >= -Math.pow(2, 63) && x <= Math.pow(2, 63) - 1) System.out.println("* long");
             } catch (Exception e) {
-                System.out.println(in.next()+" can't be fitted anywhere.");
+                System.out.println(in.next() + " can't be fitted anywhere.");
             }
         }
     }
